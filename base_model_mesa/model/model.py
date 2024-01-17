9e4b3ff5c1e9b157a4fbea8051774bf2fd026d5d
StijnWalsh <s.r.d.walsh@student.tudelft.nl>
2024-01-17 12:04:16 +0100

    ,

--- base_model_mesa/model/model.py
+++ base_model_mesa/model/model.py
@@ -73,7 +73,7 @@ class AdaptationModel(Model):
             self.schedule.add(household)
             self.grid.place_agent(agent=household, node_id=node)
         
-        self.agents.do('define_friends', radius=1)
+        self.agents.do('define_friends', radius=3)
 
         # You might want 
         # to create other agents here, e.g. insurance agents.
